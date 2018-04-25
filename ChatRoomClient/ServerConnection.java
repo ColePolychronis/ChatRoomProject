4c173d44489a93e7c6e50c4bebefbff39640ca88
Cole Polychronis <colepolychronis@Coles-MacBook-Pro-4.local>
2018-04-25 15:02:57 -0600

    stage 1 of multithreading client

--- ChatRoomClient/ServerConnection.java
+++ ChatRoomClient/ServerConnection.java
@@ -16,21 +16,60 @@ public class ServerConnection implements Runnable{
   /**
   * this method is invoked by a separate thread
   */
-  private ConcurrentHashMap<String, Socket> clientList;
-  private Vector<JSONObject> messages;
+  // private ConcurrentHashMap<String, Socket> clientList;
+  // private Vector<JSONObject> messages;
+  Socket sock;
+  Vector<String> clientList;
+  Vector<JSONObject> toServer;
+  Vector<JSONObject> fromServer;
+  String clientName;
 
-  public void process() throws java.io.IOException{
 
+  public void processOutbound(Vector<String> clientList, Vector<JSONObject> messages) throws java.io.IOException{
+    while(!messages.isEmpty()){
+      JSONObject messToServer = messages.remove(0);
+      // set up the necessary communication channels
+			PrintWriter writeToServer = new PrintWriter(this.sock.getOutputStream(),true);
+      writeToServer(messToServer.toString());
+    }
+  }
+
+  public void processInbound(Vector<String> clientList, Vector<JSONObject> messages) throws java.io.IOException{
+    while(!messages.isEmpty()){
+      JSONObject messFromServer = messages.remove(0);
+      if(messFromServer.get("type").equals("chatroom-update")){ // if we get an update message from the server
+        if(messFromServer.get("type_of_update").equals("enter")){ // if someone has joined, add them to clientList
+          clientList.add(messFromServer.get("id"));
+          System.out.println("Client " + messFromServer.get("id") + " has joined the chatroom.");
+        }
+        if(messFromServer.get("type_of_update").equals("leave")){ // if someone has left, remove them to clientList
+          clientList.remove(messFromServer.get("id"));
+          System.out.println("Client " + messFromServer.get("id") + " has left the chatroom.");
+        }
+      }
+      if(messFromServer.get("type").equals("chatroom-response")){
+        clientName = clientName.concat(":").concat(messFromServer.get("id"));
+      }
+      // set up the necessary communication channels
+			System.out.println(messFromServer.toString());
+    }
   }
 
   //Constructor for runnable method
-  public ServerConnection() {
-
+  public ServerConnection(Socket sock, Vector<String> clientList, Vector<JSONObject> toServer, Vector<JSONObject> fromServer, String clientName) {
+    this.sock = sock;
+    this.clientList = clientList;
+    this.toServer = toServer;
+    this.fromServer = fromServer;
+    this.clientName = clientName;
   }
 
   public void run() {
     try {
-      process(clientList, messages);
+      while(true){
+        processOutbound(toServer);
+        processInbound(fromServer);
+      }
     }
     catch (java.io.IOException ioe) {
       System.err.println(ioe);
